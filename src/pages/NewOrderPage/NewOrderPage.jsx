96b975f8ba692ed04dbdf9ce460dd3f57247fae5
Yi Li <youremail@domain.com>
2022-06-06 16:08:54 -0700

    create check out form

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,5 +1,22 @@
 export default function NewOrderPage() {
   return (
+    <>
     <h1>NewOrderPage</h1>
+    <div className="form-container">
+    <form >
+      <label>Instructor</label>
+      <select value="instructor">
+        <option value="JimC">Jim C</option>
+        <option value="Step">Stephanie L</option>
+        <option value="MarioM">Mario R</option>
+      </select>
+      <label>Hours: </label>
+      <input type="text" name="hours" required />
+      <label>Total Hours: </label>
+      <input type="text" name="total-hours" required />
+      <button type="submit">Check out</button>
+    </form>
+    </div>
+    </>
   );
 }
